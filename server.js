a4f430824ab9f3eda6484c77fd99562ea2ce3797
Richard Seward <raseward14@gmail.com>
2021-03-16 20:39:33 -0600

    installed dependencies

---
+++ server.js
@@ -0,0 +1,34 @@
+const express = require('express');
+
+const path = require('path');
+
+const app = express();
+
+// set initial port
+const PORT = process.env || 3000;
+
+// sets up the express app to handle data parsing
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+// * `GET *` should return the `index.html` file.
+app.get('/', (req, res) => {
+    res.sendFile(path.join(__dirname, '/public/index.html'))
+});
+
+// * `GET /notes` should return the `notes.html` file.
+
+// * `GET /api/notes` should read the `db.json` file and return all saved notes as JSON.
+
+// * `POST /api/notes` should receive a new note to save on the request body, add it to the `db.json` file, and then return the new note to the client. You'll need to find a way to give each note a unique id when it's saved (look into `npm` packages that could do this for you).
+
+// * `DELETE /api/notes/:id` should receive a query parameter containing the id of a note to delete. In order to delete a note, you'll need to read all notes from the `db.json` file, remove the note with the given `id` property, and then rewrite the notes to the `db.json` file.
+
+
+
+
+
+// starts the server to begin listening
+app.listen(PORT, () => {
+    console.log(`Listening on PORT ${PORT}`)
+});
