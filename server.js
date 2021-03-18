80f8058924412a50d7e485cf350d6f18392b237c
Richard Seward <raseward14@gmail.com>
2021-03-18 10:47:37 -0600

    C:/Program Files/Git/api/notes returns notes.html

--- server.js
+++ server.js
@@ -13,12 +13,19 @@ app.use(express.json());
 
 // * `GET *` should return the `index.html` file.
 app.get('/', (req, res) => {
-    res.sendFile(path.join(__dirname, '/public/index.html'))
+    res.sendFile(path.join(__dirname, '/public/index.html'));
 });
 
 // * `GET /notes` should return the `notes.html` file.
+app.get('/notes', (req, res) => {
+    res.sendFile(path.join(__dirname, '/public/notes.html'));
+});
 
 // * `GET /api/notes` should read the `db.json` file and return all saved notes as JSON.
+app.get('/api/notes', (req, res) => {
+    res.status(200).json();
+    console.log();
+});
 
 // * `POST /api/notes` should receive a new note to save on the request body, add it to the `db.json` file, and then return the new note to the client. You'll need to find a way to give each note a unique id when it's saved (look into `npm` packages that could do this for you).
 
